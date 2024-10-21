cd431a629af28498c187a1a900a3b8e0b8fcb37e
Erick Muke <muke7881@gmail.com>
2024-10-21 17:49:47 -0600

    fix: Se pulen pequenos detalles, se cambie LICENSE, version 1.0.0 finalizada

--- NOTES.md
+++ NOTES.md
@@ -2,3 +2,4 @@
 
 - Migrar poco a poco de js vanilla a react con dynamic islands
 - Mejorando el responsive y los stacks de proyectos
+- Agregar el paquete de idiomas en ingles y ir adecuando el codigo a ingles
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,16 +1,29 @@
 import { useState } from 'react';
 
 export default function Counter() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(1);
+  const initialValue = 1;
 
   return (
-    <div className="bg-blue-500 text-white p-4 rounded">
+    <div className="text-base-content p-4 rounded">
       <p>Count: {count}</p>
       <button
         onClick={() => setCount(count + 1)}
-        className="mt-2 px-4 py-2 bg-green-500 hover:bg-green-700 rounded"
+        className="mt-2 px-4 py-2 btn btn-primary"
       >
         Increment
+      </button>
+      <button
+        onClick={() => setCount(count - 1)}
+        className="mt-2 px-4 py-2 btn btn-primary"
+      >
+        Decrement
+      </button>
+      <button
+        onClick={() => setCount(initialValue)}
+        className="mt-2 px-4 py-2 btn btn-primary"
+      >
+        Reset
       </button>
     </div>
   );
