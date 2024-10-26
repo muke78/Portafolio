232e455176de3c154451bdc20382d5c4b548f398
Erick Muke <muke7881@gmail.com>
2024-10-25 18:53:59 -0600

    feat: Se agrego la traducion al header de la pagina

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -1,17 +1,20 @@
+import usa from '../img/usa.png';
+import mxm from '../img/mxm.png';
+
 export const languages = {
-    en: "English",
-    es: "Español",
+    en: {
+        label: "English",
+        img: usa
+    },
+    es: {
+        label: "Español",
+        img: mxm,
+    }
 };
 
 export const defaultLang = 'es';
 
 export const ui = {
-    en: {
-        'header.title': `Hello I'am`,
-        'header.about': 'Experience in the design and development of web applications, covering both frontend and backend, as well as solid knowledge of data analysis and databases.',
-    },
-    es: {
-        'header.title': 'Hola soy',
-        'header.about': 'Experiencia en el diseño y desarrollo de aplicaciones web, abarcando tanto el frontend como el backend, además de un sólido manejo de análisis de datos y bases de datos.',
-    },
+    en: {},
+    es: {},
 } as const;
