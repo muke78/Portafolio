c3346e88246c4e31f2910104d4d8894adf4b00a5
Khelde <94265348+muke78@users.noreply.github.com>
2024-11-10 01:38:22 -0600

    feat; Se agrega el modal y huella ecologica
    Se arregla la visualizacion de el cv para verlo de forma externa

---
+++ src/utils/es/dataExperiencia.ts
@@ -0,0 +1,18 @@
+export const dataExperiencia = [
+  {
+    img: "https://avatars.githubusercontent.com/u/168603899?s=200&v=4",
+    work: "Actual",
+    title: "Desarrollador full stack",
+    subtitle: "sleeplesscofi · Autónomo",
+    time: "abr. 2024 · 8 meses",
+    location: "Lima, Perú · En remoto",
+    experience: [
+      "• Organización autónoma en GitHub para desarrollar aplicaciones completas web",
+      "• Proyecto de Backend para hacer funcionar un clon de la red social de Threads ",
+      "• Proyecto frontend como vista para el usuario de un clon de Threads ",
+      "• Proyecto de un jardín de niños completo con Backend y Frontend para funciones de guardar maestros login, llevar un control de papás y estudiantes y evaluaciones ",
+      "• Proyecto de un backend task para manejar los estados de las tareas como guardar, actualizar y eliminar y traer tareas por estados según la tarea",
+      "• Single page aplication(SPA) de un banco para presentación de compañía",
+    ],
+  },
+];
