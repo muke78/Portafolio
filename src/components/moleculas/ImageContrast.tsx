68da71cf6937eb81b700bd885b8a435a9782a6ff
Khelde <94265348+muke78@users.noreply.github.com>
2025-03-01 01:46:12 -0600

    feat: Se agrega nuevo logo a la pagina

--- src/components/moleculas/ImageContrast.tsx
+++ src/components/moleculas/ImageContrast.tsx
@@ -1,5 +1,5 @@
-import kdl from "@/img/kdl.webp";
-import kdlWhite from "@/img/kdlWhite.webp";
+import kdl from "@/img/KheldeNew.svg";
+import kdlWhite from "@/img/KheldeNew.svg";
 import { darkThemes } from "@/utils/dataDarkThemes";
 
 import { useEffect, useState } from "react";
@@ -24,6 +24,7 @@ export const ImageContrast = ({ width }) => {
 
     return () => observer.disconnect();
   }, []);
+
   const imageSrc = darkThemes.includes(currentTheme) ? kdlWhite : kdl;
 
   return (
@@ -33,7 +34,7 @@ export const ImageContrast = ({ width }) => {
         alt="Logotipo personal de Erick Gonzalez"
         aria-label="Logotipo personal de Erick Gonzalez"
         loading="lazy"
-        className="transition-all duration-300 rounded-sm"
+        className={`transition-all duration-300 rounded-sm ${darkThemes.includes(currentTheme) ? 'invert opacity-80 grayscale' : ''}`}
       />
     </div>
   );
