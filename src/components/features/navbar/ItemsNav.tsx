cefb95c740a1dcbb974370b2b3b7eda14d8024a6
Khelde <94265348+muke78@users.noreply.github.com>
2025-03-04 19:24:08 -0600

    feat: Se cambia el nav a tsx se optimiza y se arregla errores

---
+++ src/components/features/navbar/ItemsNav.tsx
@@ -0,0 +1,38 @@
+import { dataListNavbarEN } from "@/utils/en/dataNavbarEN.astro";
+import { dataListNavbar } from "@/utils/es/dataNavbar.astro";
+import { dataListNavbarFR } from "@/utils/fr/dataNavbarFR.astro";
+
+import React from "react";
+
+interface NavbarItem {
+  to: string;
+  label: string;
+}
+
+interface Props {
+  currentLocale: string;
+}
+
+export const ItemsNav: React.FC<Props> = ({ currentLocale }) => {
+  const dataChange: NavbarItem[] =
+    currentLocale === "es"
+      ? dataListNavbar
+      : currentLocale === "fr"
+        ? dataListNavbarFR
+        : dataListNavbarEN;
+
+  return (
+    <>
+      {dataChange.map((list) => (
+        <li key={list.to}>
+          <a
+            href={list.to}
+            aria-label={`Ir a ${list.label}`}
+          >
+            {list.label}
+          </a>
+        </li>
+      ))}
+    </>
+  );
+};
---
+++ src/components/organisms/Nav.tsx
@@ -0,0 +1,99 @@
+import React, { useEffect, useState } from "react";
+
+import { motion } from "framer-motion";
+import { Menu, X } from "lucide-react";
+
+import { ItemsNav } from "../features/navbar/ItemsNav";
+import { ImageContrast } from "../utils/ImageContrast";
+
+export const Nav = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const toggleSidebar = () => setIsOpen(!isOpen);
+  const closeSidebar = () => setIsOpen(false);
+
+  useEffect(() => {
+    const savedTheme = localStorage.getItem("theme");
+    if (savedTheme) {
+      document.documentElement.setAttribute("data-theme", savedTheme);
+    }
+
+    const themeButtons = document.querySelectorAll("[data-set-theme]");
+
+    const handleThemeClick = (event: Event) => {
+      const button = event.currentTarget as HTMLElement;
+      const theme = button.getAttribute("data-set-theme");
+      localStorage.setItem("theme", theme || "");
+      document.documentElement.setAttribute("data-theme", theme || "");
+    };
+
+    themeButtons.forEach((button) => {
+      button.addEventListener("click", handleThemeClick);
+    });
+    return () => {
+      themeButtons.forEach((button) => {
+        button.removeEventListener("click", handleThemeClick);
+      });
+    };
+  }, []);
+
+  return (
+    <nav className="navbar bg-base-100 rounded-b-lg py-5 px-5 sticky top-0 left-0 w-full z-50  shadow-md flex items-center">
+      {/* Logo */}
+      <div className="flex-grow items-center gap-4">
+        <ImageContrast width="w-24" />
+      </div>
+      <div className="flex gap-3">
+        {/* Navbar Principal */}
+        <ul className="hidden min-[600px]:flex space-x-6">
+          <ItemsNav currentLocale={"es"} />
+        </ul>
+
+        {/* Botón para Cambiar Tema */}
+        <button className="btn btn-ghost">Theme</button>
+
+        {/* Selector de Idioma */}
+        <button className="btn btn-ghost">Lang</button>
+
+        {/* Botón Menú para Mobile */}
+        <button
+          onClick={toggleSidebar}
+          className="min-[600px]:hidden btn btn-ghost text-base"
+        >
+          <Menu size={24} />
+        </button>
+      </div>
+
+      {/* Overlay para cerrar sidebar al hacer clic fuera */}
+      {isOpen && (
+        <div
+          className="fixed inset-0 bg-black bg-opacity-50 z-20"
+          onClick={closeSidebar}
+        ></div>
+      )}
+
+      {/* Sidebar */}
+      <motion.div
+        initial={{ x: "100%" }}
+        animate={{ x: isOpen ? 0 : "100%" }}
+        transition={{ type: "tween", duration: 0.3 }}
+        className={`fixed top-0 right-0 h-full w-64 bg-primary shadow-lg z-50 p-4 ${isOpen ? "pointer-events-auto" : "pointer-events-none"}`}
+      >
+        {/* Botón para cerrar */}
+        <button
+          onClick={closeSidebar}
+          className="absolute top-4 right-4 btn btn-ghost dark:text-white text-white"
+        >
+          <X size={24} />
+        </button>
+
+        {/* Contenido del Sidebar */}
+        <div className="w-full  text-3xl">
+          <ul className="space-y-4 flex flex-col justify-center items-center gap-8 dark:text-white text-white">
+            <ItemsNav currentLocale={"es"} />
+          </ul>
+        </div>
+      </motion.div>
+    </nav>
+  );
+};
